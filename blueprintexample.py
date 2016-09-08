f171223fbe2eee77fce1fede576e46910c1ea802
smshiv <smshiv@gmail.com>
2016-09-08 16:16:03 +0530

    Update blueprintexample.py

--- blueprintexample.py
+++ blueprintexample.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from simple_page.simple_page import simple_page
-
+#Shiv
 app = Flask(__name__)
 app.register_blueprint(simple_page)
 # Blueprint can be registered many times
